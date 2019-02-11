0871730bf12f4b04a6c70f2ea0c29d1beafa3c39
Mybeard <alexeymayboroda23@gmail.com>
2019-02-11 17:18:30 +0200

    diamond to CLI

---
+++ Diamond.php
@@ -0,0 +1,26 @@
+<?php
+
+$diagonal = $argv[1];
+
+if(!(isset($diagonal))) {
+    exit("Ne zadan razmer diagonali");
+}
+
+for ($i = 1; $i < $diagonal; $i++) {
+    for ($j = $i; $j < $diagonal; $j++) {
+        echo "  ";
+    }
+    for ($j = 2 * $i - 1; $j > 0; $j--) {
+        echo (" *");
+    }
+    echo PHP_EOL;
+}
+for ($i = $diagonal; $i > 0; $i--) {
+    for ($j = $diagonal - $i; $j > 0; $j--) {
+        echo "  ";
+    }
+    for ($j = 2 * $i - 1; $j > 0; $j--) {
+        echo (" *");
+    }
+    echo PHP_EOL;
+}
