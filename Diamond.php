7bc8476ef7c22e9069f66fdcc6d7172bc238de07
Mybeard <alexeymayboroda23@gmail.com>
2019-02-11 17:22:54 +0200

    minor changes

--- Diamond.php
+++ Diamond.php
@@ -8,19 +8,19 @@ if(!(isset($diagonal))) {
 
 for ($i = 1; $i < $diagonal; $i++) {
     for ($j = $i; $j < $diagonal; $j++) {
-        echo "  ";
+        echo " ";
     }
     for ($j = 2 * $i - 1; $j > 0; $j--) {
-        echo (" *");
+        echo ("*");
     }
     echo PHP_EOL;
 }
 for ($i = $diagonal; $i > 0; $i--) {
     for ($j = $diagonal - $i; $j > 0; $j--) {
-        echo "  ";
+        echo " ";
     }
     for ($j = 2 * $i - 1; $j > 0; $j--) {
-        echo (" *");
+        echo ("*");
     }
     echo PHP_EOL;
 }
